70956c227e9ccc11b2e18e09a8c7a3ee084abfc0
AbdellahELK <abdellahelkhald@gmail.com>
2025-02-17 23:13:30 +0100

    create user model with fields validation

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,12 +6,9 @@ export const connectToDb = async () => {
             throw new Error("MONGO_URI is not defined in environment variables.");
         }
 
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log("MongoDB Connected Successfully");
+        console.log("MongoDB Connected Successfully " + conn.connection.host);
     } catch (error) {
         console.error("MongoDB Connection Error:", error.message);
         process.exit(1); // Exit process with failure
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,18 @@
-import express from 'express'
+import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDb } from './config/db.js';
 import authRoutes from './routes/auth.routes.js';
 
-
 dotenv.config();
 
-
 const app = express();
-app.use(express.json())
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+const port = process.env.PORT;
 
-
-const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
     connectToDb();
