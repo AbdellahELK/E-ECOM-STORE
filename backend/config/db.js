eb5a0e83dad169e916571f9e29483826efc8ff14
AbdellahELK <abdellahelkhald@gmail.com>
2025-02-18 23:01:58 +0100

    create auth endpoints

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 export const connectToDb = async () => {
     try {
-        if (!process.env.MONGO_URI) {
-            throw new Error("MONGO_URI is not defined in environment variables.");
-        }
-
         const conn = await mongoose.connect(process.env.MONGO_URI);
-
         console.log("MongoDB Connected Successfully " + conn.connection.host);
     } catch (error) {
         console.error("MongoDB Connection Error:", error.message);
         process.exit(1); // Exit process with failure
     }
 };
+
+// alimaaloulabde16
+
+
+// T65UfbcBlysQCuhu
